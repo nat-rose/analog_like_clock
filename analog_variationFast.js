f44977f7795286b1184ad40bc88387fdf5b2c1d3
nat-rose <78227850+nat-rose@users.noreply.github.com>
2021-09-17 14:48:40 +0200

    Create analog_variationFast.js

---
+++ analog_variationFast.js
@@ -0,0 +1,31 @@
+const UPDATE_INTERVAL_MILLIS = 1250
+ // changes speed of time 1000 to 1250 ms
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+const shownDate = new Date()
+
+function clockTick() {
+    // Advance one more second
+    shownDate.setSeconds(shownDate.getSeconds() + 1)
+
+    updateClockView()
+}
+
+function updateClockView() {
+    const secondsRatio = shownDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + shownDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + shownDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+var ticks = setInterval (clockTick, UPDATE_INTERVAL_MILLIS)
+clockTick()
